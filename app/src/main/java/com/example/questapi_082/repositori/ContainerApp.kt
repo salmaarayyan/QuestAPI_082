49c9e391c132a202a1773df49e8efc98a5389450
salmaarayyan <salmaarayyan@gmail.com>
2025-12-18 12:18:31 +0700

    Menambahkan konfigurasi Retrofit dengan Serialization dan OkHttp pada ContainerApp.kt

--- app/src/main/java/com/example/questapi_082/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questapi_082/repositori/ContainerApp.kt
@@ -1,7 +1,11 @@
 package com.example.questapi_082.repositori
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
 
 interface ContainerApp{
     val repositorySiswa: RepositoryDataSiswa
@@ -19,5 +23,17 @@ class DefaultContainerApp : ContainerApp{
         .addInterceptor(logging)
         .build()
 
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(baseurl)
+        .addConverterFactory(
+            Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            }.asConverterFactory("application/json".toMediaType())
+        )
+        .client(klien)
+        .build()
 
 }
+
