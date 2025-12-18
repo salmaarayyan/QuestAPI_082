7f95cb872d1434b780fee70f1c7482590d6eb392
salmaarayyan <salmaarayyan@gmail.com>
2025-12-18 12:28:36 +0700

    Menambahkan class HomeViewModel beserta logika inisialisasi pemuatan data siswa dan state StatusUiSiswa pada HomeViewModel.kt

--- app/src/main/java/com/example/questapi_082/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/questapi_082/viewmodel/HomeViewModel.kt
@@ -1,9 +1,23 @@
 package com.example.questapi_082.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.questapi_082.modeldata.DataSiswa
+import com.example.questapi_082.repositori.RepositoryDataSiswa
 
 sealed interface StatusUiSiswa{
     data class Success(val siswa: List<DataSiswa>) : StatusUiSiswa
     object Error : StatusUiSiswa
     object Loading : StatusUiSiswa
+}
+
+class HomeViewModel(private val repositorySiswa: RepositoryDataSiswa) : ViewModel() {
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init{
+        loadSiswa()
+    }
 }
