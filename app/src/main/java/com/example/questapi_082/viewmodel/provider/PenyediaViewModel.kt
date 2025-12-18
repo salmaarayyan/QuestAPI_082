357860e7fd1d0847509c7165ba5d27b7eeb508d3
salmaarayyan <salmaarayyan@gmail.com>
2025-12-18 12:56:53 +0700

    Menambahkan file PenyediaViewModel.kt untuk mengelola factory HomeViewModel dan EntryViewModel

---
+++ app/src/main/java/com/example/questapi_082/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.questapi_082.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.questapi_082.repositori.AplikasiDataSiswa
+import com.example.questapi_082.viewmodel.EntryViewModel
+import com.example.questapi_082.viewmodel.HomeViewModel
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
+        )
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().containerApp.repositoryDataSiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().containerApp.repositoryDataSiswa) }
+    }
+}
