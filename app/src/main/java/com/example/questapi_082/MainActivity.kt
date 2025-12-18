1ef0f7a2ebfab0b4b3fc0da8dc7b4dc91b1d9400
salmaarayyan <salmaarayyan@gmail.com>
2025-12-18 14:12:32 +0700

    Mengubah konten utama pada MainActivity.kt dengan memanggil composable DataSiswaApp dan menghapus fungsi Greeting bawaan project baru

--- app/src/main/java/com/example/questapi_082/MainActivity.kt
+++ app/src/main/java/com/example/questapi_082/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questapi_082.ui.theme.QuestAPI_082Theme
+import com.example.questapi_082.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAPI_082Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestAPI_082Theme {
-        Greeting("Android")
     }
 }
