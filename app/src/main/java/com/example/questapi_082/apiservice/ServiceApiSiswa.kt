8105c4399afa670759cf9ef56f1ea89511551d64
salmaarayyan <salmaarayyan@gmail.com>
2025-12-18 12:03:15 +0700

    Menambahkan method POST untuk fungsi simpan data siswa pada interface ServiceApiSiswa

--- app/src/main/java/com/example/questapi_082/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_082/apiservice/ServiceApiSiswa.kt
@@ -1,10 +1,16 @@
 package com.example.questapi_082.apiservice
 
 import com.example.questapi_082.modeldata.DataSiswa
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ServiceApiSiswa{
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun  postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 
 
 }
