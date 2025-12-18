d736107b1701e6ac5de04c092514b137a0a70f9a
salmaarayyan <salmaarayyan@gmail.com>
2025-12-18 12:35:17 +0700

    Menambahkan logika pengambilan data siswa menggunakan Coroutines dan penanganan error pada HomeViewModel.kt

--- app/src/main/java/com/example/questapi_082/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/questapi_082/viewmodel/HomeViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.questapi_082.viewmodel
 
+import android.net.http.HttpException
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.questapi_082.modeldata.DataSiswa
 import com.example.questapi_082.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed interface StatusUiSiswa{
     data class Success(val siswa: List<DataSiswa>) : StatusUiSiswa
@@ -13,11 +17,24 @@ sealed interface StatusUiSiswa{
     object Loading : StatusUiSiswa
 }
 
-class HomeViewModel(private val repositorySiswa: RepositoryDataSiswa) : ViewModel() {
+class HomeViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
     var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
         private set
 
-    init{
+    init {
         loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            listSiswa = StatusUiSiswa.Loading
+            listSiswa = try {
+                StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
+            } catch (e: IOException) {
+                StatusUiSiswa.Error
+            } catch (e: HttpException) {
+                StatusUiSiswa.Error
+            }
+        }
     }
 }
