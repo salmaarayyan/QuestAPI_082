5ec84c795d2ac7f8db6d1558702d71a0a94f2712
salmaarayyan <salmaarayyan@gmail.com>
2025-12-18 12:15:09 +0700

    Menambahkan kelas `DefaultContainerApp` yang mengimplementasikan `ContainerApp` serta konfigurasi `HttpLoggingInterceptor` dan `baseurl` pada `ContainerApp.kt`

--- app/src/main/java/com/example/questapi_082/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questapi_082/repositori/ContainerApp.kt
@@ -1,6 +1,16 @@
 package com.example.questapi_082.repositori
 
+import okhttp3.logging.HttpLoggingInterceptor
+
 interface ContainerApp{
     val repositorySiswa: RepositoryDataSiswa
 }
 
+class DefaultContainerApp : ContainerApp{
+    private val baseurl = "http://10.0.2.2/tiumy/"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level= HttpLoggingInterceptor.Level.BODY
+
+    }
+}
