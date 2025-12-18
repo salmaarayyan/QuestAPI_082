5802e866a82c0180b98b1fa1051f502470301fcb
salmaarayyan <salmaarayyan@gmail.com>
2025-12-18 12:08:11 +0700

    Menambahkan implementasi `JaringanRepositoryDataSiswa` pada `RepositoryDataSiswa.kt`

--- app/src/main/java/com/example/questapi_082/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_082/repositori/RepositoryDataSiswa.kt
@@ -1,5 +1,6 @@
 package com.example.questapi_082.repositori
 
+import com.example.questapi_082.apiservice.ServiceApiSiswa
 import com.example.questapi_082.modeldata.DataSiswa
 
 interface RepositoryDataSiswa{
@@ -7,3 +8,10 @@ interface RepositoryDataSiswa{
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
 }
 
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+):RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+
+}
