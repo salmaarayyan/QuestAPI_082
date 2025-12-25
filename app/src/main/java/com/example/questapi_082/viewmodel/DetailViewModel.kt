b142ad8b3d7df1d77c6a78982d2668eed5ef3e86
salmaarayyan <salmaarayyan@gmail.com>
2025-12-25 13:29:58 +0700

    Menambahkan implementasi class DetailViewModel serta mendefinisikan StatusUIDetail pada DetailViewModel.kt

--- app/src/main/java/com/example/questapi_082/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_082/viewmodel/DetailViewModel.kt
@@ -1,6 +1,12 @@
 package com.example.questapi_082.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.questapi_082.modeldata.DataSiswa
+import com.example.questapi_082.repositori.RepositoryDataSiswa
 import kotlinx.serialization.InternalSerializationApi
 
 @file:OptIn(InternalSerializationApi::class)
@@ -9,4 +15,10 @@ sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
+}
+class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
 }
