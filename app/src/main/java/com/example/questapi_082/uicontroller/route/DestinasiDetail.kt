75ba96c3743f22217e6bfa24302a2321e62fcd83
salmaarayyan <salmaarayyan@gmail.com>
2025-12-25 15:08:50 +0700

    Mengupdate konfigurasi rute navigasi dan argumen ID pada DestinasiDetail dan DestinasiEdit

--- app/src/main/java/com/example/questapi_082/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_082/uicontroller/route/DestinasiDetail.kt
@@ -2,9 +2,10 @@ package com.example.questapi_082.uicontroller.route
 
 import com.example.questapi_082.R
 
-object DestinasiDetail : DestinasiNavigasi {
-    override val route = "detail"
+object DestinasiDetail: DestinasiNavigasi {
+    override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
-    const val itemIdArg = "id"
-    val routeWithArgs = "$route/{IdArgs}"
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
+
--- app/src/main/java/com/example/questapi_082/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questapi_082/uicontroller/route/DestinasiEdit.kt
@@ -2,9 +2,9 @@ package com.example.questapi_082.uicontroller.route
 
 import com.example.questapi_082.R
 
-object DestinasiEdit : DestinasiNavigasi {
-    override val route = "edit"
+object DestinasiEdit: DestinasiNavigasi {
+    override val route = "item_edit"
     override val titleRes = R.string.edit_siswa
-    const val itemIdArg = "id"
-    val routeWithArgs = "${DestinasiDetail.route}/{IdArgs}"
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
