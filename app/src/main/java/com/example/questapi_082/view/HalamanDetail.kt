9365241bdca58e9b0a992fcb438888dc48a49f32
salmaarayyan <salmaarayyan@gmail.com>
2025-12-25 13:41:27 +0700

    Membuat file HalamanDetail.kt dan menambahkan fungsi composable DetailSiswaScreen serta integrasi DetailViewModel

--- app/src/main/java/com/example/questapi_082/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_082/view/HalamanDetail.kt
@@ -1,2 +1,19 @@
 package com.example.questapi_082.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_082.viewmodel.DetailViewModel
+import com.example.questapi_082.viewmodel.provider.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+}
