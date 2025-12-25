d0bd11a1ed3aa5df4b30c2dc3423e77b6c76fbde
salmaarayyan <salmaarayyan@gmail.com>
2025-12-25 13:04:27 +0700

    Menambahkan fungsi getSatuSiswa, editSatuSiswa, dan hapusSatuSiswa pada RepositoryDataSiswa.kt

--- app/src/main/java/com/example/questapi_082/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_082/repositori/RepositoryDataSiswa.kt
@@ -6,6 +6,9 @@ import com.example.questapi_082.modeldata.DataSiswa
 interface RepositoryDataSiswa{
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun editSatuSiswa(id:Int,dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
