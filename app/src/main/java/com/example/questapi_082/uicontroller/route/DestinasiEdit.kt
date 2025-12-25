8b823a64649e6dccbb3d75492ec1f7105501d54e
salmaarayyan <salmaarayyan@gmail.com>
2025-12-25 15:06:13 +0700

    Menambahkan objek `DestinasiEdit` pada `DestinasiEdit.kt` untuk keperluan navigasi fitur edit data siswa

--- app/src/main/java/com/example/questapi_082/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questapi_082/uicontroller/route/DestinasiEdit.kt
@@ -1,2 +1,10 @@
 package com.example.questapi_082.uicontroller.route
 
+import com.example.questapi_082.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "id"
+    val routeWithArgs = "${DestinasiDetail.route}/{IdArgs}"
+}
