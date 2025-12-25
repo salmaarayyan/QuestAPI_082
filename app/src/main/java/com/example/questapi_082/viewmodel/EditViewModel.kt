159651629fc281cf87e81bfcb123cf87b47a3feb
salmaarayyan <salmaarayyan@gmail.com>
2025-12-25 13:19:36 +0700

    Menambahkan implementasi logic pada EditViewModel untuk mengambil data siswa berdasarkan ID dari repository

--- app/src/main/java/com/example/questapi_082/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_082/viewmodel/EditViewModel.kt
@@ -1,2 +1,21 @@
 package com.example.questapi_082.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import com.example.questapi_082.modeldata.UIStateSiswa
+import com.example.questapi_082.repositori.RepositoryDataSiswa
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
+}
