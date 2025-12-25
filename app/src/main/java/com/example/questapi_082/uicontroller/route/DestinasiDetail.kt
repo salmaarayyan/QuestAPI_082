a3f8e2a7bde4dae8903aed2518f625ddc30dec91
salmaarayyan <salmaarayyan@gmail.com>
2025-12-25 15:06:01 +0700

    Menambahkan object `DestinasiDetail` untuk navigasi detail siswa pada `DestinasiDetail.kt`

--- app/src/main/java/com/example/questapi_082/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_082/uicontroller/route/DestinasiDetail.kt
@@ -1,2 +1,10 @@
 package com.example.questapi_082.uicontroller.route
 
+import com.example.questapi_082.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "id"
+    val routeWithArgs = "$route/{IdArgs}"
+}
