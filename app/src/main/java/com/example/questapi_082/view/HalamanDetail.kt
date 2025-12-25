4b0000fdec6878560ccd95a82fe5fc62eec1d14e
salmaarayyan <salmaarayyan@gmail.com>
2025-12-25 13:48:23 +0700

    Menambahkan FloatingActionButton pada HalamanDetail.kt untuk navigasi ke halaman edit item

--- app/src/main/java/com/example/questapi_082/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_082/view/HalamanDetail.kt
@@ -1,10 +1,13 @@
 package com.example.questapi_082.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_082.R
 import com.example.questapi_082.viewmodel.DetailViewModel
 import com.example.questapi_082.viewmodel.provider.PenyediaViewModel
 
@@ -24,6 +27,15 @@ fun DetailSiswaScreen(
                 navigateUp = navigateBack
             )
         },
+        floatingActionButton = {
+            val uiState = viewModel.statusUIDetail
+            FloatingActionButton(
+                onClick = {
+                    when(uiState){ is StatusUIDetail.Success->navigateToEditItem(uiState.satatusiswa.id) else->{}}
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(R.dimen.padding_large))
+        }
 
 
     )
