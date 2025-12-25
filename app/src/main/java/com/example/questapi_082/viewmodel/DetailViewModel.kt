45d305395f3f3724695ae01fe1ccae16631418ee
salmaarayyan <salmaarayyan@gmail.com>
2025-12-25 13:27:56 +0700

    Menambahkan sealed interface StatusUIDetail untuk mengelola status UI detail siswa pada DetailViewModel.kt

--- app/src/main/java/com/example/questapi_082/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_082/viewmodel/DetailViewModel.kt
@@ -1,2 +1,12 @@
 package com.example.questapi_082.viewmodel
 
+import com.example.questapi_082.modeldata.DataSiswa
+import kotlinx.serialization.InternalSerializationApi
+
+@file:OptIn(InternalSerializationApi::class)
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
